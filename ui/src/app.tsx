aca8563ae31024f8ad1244f33a9ff1983536535c
sink666 <er.basilisk@gmail.com>
2022-09-10 21:42:11 +0100

    function to return list of actions; display flow rates, flow rate component

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -7,6 +7,55 @@ const api = new Urbit('', '', window.desk);
 api.ship = window.ship;
 window.api = api;
 
+function RateForm() {
+  const [flowRate, setFlowrate] = useState();
+  const [rateDate, setRatedate] = useState();
+
+  let maxdate = dayjs()
+  let mindate = dayjs().subtract(40, "days");
+
+  const submitFlowrate = (timestamp) => {
+    return window.api.poke({
+        app: "full-stop",
+        mark: "dot-point",
+        json: [{ wen: timestamp, rate: {wen: rateDate, how: flowRate}}],
+      }).then(() => location.reload());
+  };
+
+  const validateSubmission = (event) => {
+    event.preventDefault();
+
+    let start = true;
+    let maxdateUnix = maxdate.add(1, "day").unix();
+    let mindateUnix = mindate.unix();
+    let timeNow = Math.floor(Date.now() / 1000);
+
+    if (typeof rateDate == 'undefined') { start = false }
+    if (rateDate > maxdateUnix || rateDate < mindateUnix) { start = false; }
+
+    if (!start ) {
+      return window.alert("you must submit a valid date for this rating, within the past 40 days")
+    }
+
+    return submitFlowrate(timeNow);
+  }
+
+  return (
+    <form onSubmit={event => validateSubmission(event)}>
+      <label>date:<br/>
+        <input type="date" className='border mb-3' min={mindate.format('YYYY-MM-DD')} max={maxdate.format('YYYY-MM-DD')} onChange={e => setRatedate(e.target.valueAsNumber / 1000)}/>
+      </label>
+      <br/>
+      <label>flow rate (5 being heavy):<br/>
+        <input type="range" className='border mb-3' min={1} max={5} defaultValue={3} onChange={ e => setFlowrate(e.target.valueAsNumber) }/>
+      </label>
+      <br/>
+      <input type="submit" value="record"/>
+    </form>
+  )
+
+}
+
 function PeriodForm() {
   const [flowDate, setFlowdate] = useState();
   const [stopDate, setStopdate] = useState();
@@ -18,7 +67,7 @@ function PeriodForm() {
     let actions = []
 
     if (pstart) { 
-      actions.push({ wen: timestamp, flow: { wen: flowDate } }); 
+      actions.push({ wen: timestamp, flow: { wen: flowDate } });
       timestamp++ 
     }
     
@@ -55,7 +104,7 @@ function PeriodForm() {
   }
 
   return (
-    <form onSubmit={event => validateSubmission(event)}>
+    <form onSubmit={event => validateSubmission(event)} className='mr-6'>
       <label>period start:<br/>
         <input type="date" className='border mb-3' min={mindate.format('YYYY-MM-DD')} max={maxdate.format('YYYY-MM-DD')} onChange={e => setFlowdate(e.target.valueAsNumber / 1000)}/>
       </label>
@@ -69,15 +118,51 @@ function PeriodForm() {
   )
 }
 
+function retActions(scryArray) {
+  let actions = []
+  let actionTemplate = (timestamp, type, obj) => { 
+    return {
+      wen: timestamp,
+      [type]: obj
+    }
+  }
+  let timeNow = Math.floor(Date.now() / 1000);
+
+  for (let period of scryArray) {
+    let { flow: { edit, rate, stop}, start } = period;
+
+    actions.push(actionTemplate(timeNow++, 'flow', {wen: start}))
+    actions.push(actionTemplate(timeNow++, 'stop', {wen: stop}))
+    if (rate.length > 0) {
+      for (let tuples of rate) {
+        let [recordDate, rating] = tuples;
+        actions.push(actionTemplate(timeNow++, 'rate', {wen: recordDate, how: rating}))
+      }
+    }
+  }
+
+  return actions
+}
+
 function retDate(v) {
-  let rv;
-  if (v == null){
-    rv = 'Not recorded';
-  } else {
+  let rv = 'not recorded';
+  if (v !== null) {
     rv = dayjs(v * 1000).format('DD/MM/YYYY').toString()
   }
   
   return rv;
+}
+
+function retRateString(tuples) {
+  let RateString = 'not recorded'
+  if (tuples.length > 0) {
+    RateString = ''
+    for (let pair of tuples) {
+      RateString += `${retDate(pair[0])} : ${pair[1]} ; `
+    }
+  }
+
+  return RateString
 }
 
 export function App() {
@@ -94,7 +179,7 @@ export function App() {
     init();
   }, [])
 
-  console.log(periods)
+  if (periods != undefined) { console.log(retActions(periods)); }
 
   return (
     <main>
@@ -104,23 +189,29 @@ export function App() {
             <th className='border pr-6'>date entered</th>
             <th className='border pr-6'>period started</th>
             <th className='border pr-6'>period ended</th>
+            <th className='border pr-6'>flow rate</th>
           </tr>
         </thead>
         <tbody>
           {Object.values(periods || {})
           .map((each) => {
+            let rateString = retRateString(each.flow.rate)
+
             return(
             <tr>
               <td className='border pr-6'>{ retDate(each?.flow?.edit) }</td>
               <td className='border pr-6'>{ retDate(each?.start) }</td>
               <td className='border pr-6'>{ retDate(each?.flow?.stop) }</td>
+              <td className='border pr-6'>{ rateString }</td>
             </tr>)
           })}
         </tbody>
       </table>
+      <p className='ml-6 my-3'>flow key: <br/> [date] : [rating] ; <br/> '1' is light, '5' is heavy </p>
       <hr/>
-      <div className='ml-6 my-3'>
+      <div className='ml-6 my-3 inline-flex'>
           <PeriodForm/>
+          <RateForm/>
       </div>
     </main>
   )
