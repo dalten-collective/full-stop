a3d28f696e090cd05c7cc44c71320fdf73261374
sink666 <er.basilisk@gmail.com>
2023-01-26 20:08:14 +0000

    grey out options for time being

--- ui/src/components/optioncomponent.tsx
+++ ui/src/components/optioncomponent.tsx
@@ -19,7 +19,6 @@ function OptionComponent({data, dispatch}) {
 
     let handleNotifSwitch = next => {
         setNotifState(next);
-        dispatch()
     };
     let handleFertiSwitch = next => {
         setFertiState(next);
@@ -30,13 +29,13 @@ function OptionComponent({data, dispatch}) {
     return (
         <>
             <div className="space-y-3">
-                <div className="font-bold text-lg">
+                <div className="font-bold text-lg text-gray-400">
                     Notifications?
-                    <ReactSwitch onChange={handleNotifSwitch} checked={notifState} className={switchstyle}/>
+                    <ReactSwitch onChange={handleNotifSwitch} checked={notifState} className={switchstyle} disabled/>
                 </div>
-                <div className="font-bold text-lg">
+                <div className="font-bold text-lg text-gray-400">
                     Fertility Predictions?
-                    <ReactSwitch onChange={handleFertiSwitch} checked={fertiState} className={switchstyle}/>
+                    <ReactSwitch onChange={handleFertiSwitch} checked={fertiState} className={switchstyle} disabled/>
                 </div>
             </div>
         </>
