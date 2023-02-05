5b1f000f1bd24c096c185cca50087f7ba3307260
sink666 <er.basilisk@gmail.com>
2023-02-05 21:14:32 +0000

    fix this again

--- ui/src/components/calendarcomponent.tsx
+++ ui/src/components/calendarcomponent.tsx
@@ -70,7 +70,7 @@ function CalendarComponent({ data, dispatch }) {
       for (let i = 0; i < prevCells.length; i++) {
         let newCell = { ...prevCells[i] };
         if (
-          isWithinPeriod(i , cPeriodData.periodStart, cPeriodData.periodStop)
+          isWithinPeriod(i + 1, cPeriodData.periodStart, cPeriodData.periodStop)
         ) {
           periodLen++;
           if (periodLen < 12) {
@@ -79,24 +79,24 @@ function CalendarComponent({ data, dispatch }) {
           }
         }
 
-        if (cPeriodData.periodStart.date() === i) {
+        if (cPeriodData.periodStart.date() === i + 1) {
           newCell.periodStart = true;
         } else if (
-          cPeriodData.periodStop.date() === i &&
+          cPeriodData.periodStop.date() === i + 1 &&
           cPeriodData.periodStop != 0
         ) {
           newCell.periodEnd = true;
         }
 
         for (let j = 0; j < cPeriodData.ratings.length; j++) {
-          if (i === cPeriodData.ratings[j].ratingDate.date()) {
+          if (i + 1 === cPeriodData.ratings[j].ratingDate.date()) {
             newCell.rating = cPeriodData.ratings[j].rating;
           }
         }
 
         for (let j = 0; j < cSpotData.length; j++) {
           let date = dayjs.unix(cSpotData[j]).date();
-          if (date === i) {
+          if (date === i + 1) {
             newCell.spot = true;
           }
         }
